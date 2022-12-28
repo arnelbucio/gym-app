1464d513b1e418973e53e90abd49830f91f91255
Arnel Bucio <rnlbucio@gmail.com>
2022-12-28 22:28:32 +0800

    Fix mobile menu modal z-index

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -20,7 +20,7 @@ const NavBar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
   return (
     <nav>
       <div
-        className={`${navbarBackground} flex-between fixed top-0 z-50 w-full py-6`}
+        className={`${navbarBackground} flex-between fixed top-0 z-40 w-full py-6`}
       >
         <div className="flex-between mx-auto w-5/6">
           <div className="flex-between w-full gap-16">
@@ -73,7 +73,7 @@ const NavBar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
 
       {/* Mobile menu modal */}
       {!isAboveMediumScreens && isMenuToggled && (
-        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+        <div className="fixed right-0 bottom-0 z-50 h-full w-[300px] bg-primary-100 drop-shadow-xl">
           {/* Close button */}
           <div className="flex justify-end p-12">
             <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
